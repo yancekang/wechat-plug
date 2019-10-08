7c99e1d41723fd735d735ff622bca8fb9ffa5741
yangjianxin <150018405@qq.com>
2019-10-08 15:56:04 +0800

    样式修改

--- pages/log/log.js
+++ pages/log/log.js
@@ -1,78 +1,66 @@
+// pages/log/log.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    
+
   },
-  asdf(e) {
-    console.log(e)
-  },
-  copyGitHub(){
-    wx.setClipboardData({
-      data: 'https://git.weixin.qq.com/yangon/wechat-plug.git',
-      success: function(){
-        wx.showToast({
-          icon: 'none',
-          title: '项目地址复制成功！',
-        })
-      }
-    })
-  },
+
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
+
   }
 })
